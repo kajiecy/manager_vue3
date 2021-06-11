a2fd9b399d245efc90e9bb1fa400ea847aae3a79
liumengxiao2014@sina.com <liumengxiao2014@sina.com>
2021-06-11 15:51:35 +0800

    测试

--- yueliao-ui/babel.config.js
+++ yueliao-ui/babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
       // babel-plugin-dynamic-import-node plugin only does one thing by converting all import() to require().
       // This plugin can significantly increase the speed of hot updates, when you have a large number of pages.
       'plugins': ['dynamic-import-node']
+      
     }
   }
 }
